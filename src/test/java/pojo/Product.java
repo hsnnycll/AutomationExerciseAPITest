f24900fefdd63706197dd1fd0328871582cfb566
Hasan YUCEL <hsnnycll@gmail.com>
2023-03-28 17:06:26 +0300

    add new file

---
+++ src/test/java/pojo/Product.java
@@ -0,0 +1,36 @@
+package pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonPropertyOrder({
+        "id",
+        "name",
+        "price",
+        "brand",
+        "category"
+})
+public class Product {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("price")
+    private String price;
+
+    @JsonProperty("brand")
+    private String brand;
+
+    @JsonProperty("category")
+    private Category category;
+}
